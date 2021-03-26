d40a53ff2605ebe76fba64f5b7c451a2cb873274
CaesarMS <caesar28997@gmail.com>
2021-03-26 08:47:21 +0700

    Config the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: 'development',
-  entry: "./src/index.js",
+  devtool: 'source-map',
+  entry: "./src/js/index.js",
   output: {
-    filename: "index.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html',
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    })
+    // new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
   ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  // devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
